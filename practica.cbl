3e50ba70f852b182b45d26911db22d24016c83a6
ZeroGravityClone <maodos356@gmail.com>
2026-09-08 00:14:49 -0700

    Update COBOL practice program
    This change updates the COBOL sample program by setting the author and purpose, renaming the program, and adding numeric variables to read two inputs and display their sum.

--- practica.cbl
+++ practica.cbl
@@ -1,16 +1,26 @@
       ***********************************************
-      * Author: Tu Nombre
+      * Author: 8a
       * Date: 2026-09-08
-      * Purpose: Aprender comentarios estructurados
+      * Purpose: Aprender COBOL desde CERO
       * Tectonics: cobc
       ***********************************************
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. HELLO-WORLD.
+       PROGRAM-ID. practica.
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
+         01 var1   PICTURE 9(4).
+         01 var2   PIC 9(4).
+         01 resul  PIC 9(6).
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
-           DISPLAY "HOLA MUNDO"
+            DISPLAY "Ingrese Variable".
+            ACCEPT var1.
+             DISPLAY var1.
+            DISPLAY "Ingrese Variable".
+            ACCEPT var2.
+             DISPLAY var2.
+             ADD var1, var2, GIVING resul.
+             DISPLAY "El resultado de la suma es: ", resul.
            STOP RUN.
-       END PROGRAM HELLO-WORLD.
+       END PROGRAM practica.
