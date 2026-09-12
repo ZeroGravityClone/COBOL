a8a0a5731820dd1da3fddc2c6bd169f232792948
ZeroGravityClone <maodos356@gmail.com>
2026-09-11 23:14:05 -0700

    Organize COBOL course files
    Moved the COBOL practice files into the Curso parte 1 COBOL BASICO directory and added nuebus.cbl as a new starter program. This keeps the course materials grouped together and makes the project structure easier to navigate.

---
+++ nuebus.cbl
@@ -0,0 +1,16 @@
+      ***********************************************
+      * Author: 8a
+      * Date: 2026-09-08
+      * Purpose: Aprender COBOL desde CERO
+      * Tectonics: cobc
+      ***********************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. practica.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            
+           STOP RUN.
+       END PROGRAM practica.
